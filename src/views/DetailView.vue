71ddd4efd4e80f0dd3f71c979f3b1e9ce4802772
Mochrizaldianandakurnia <mochrizaldianandakurnia21@gmail.com>
2022-05-25 16:59:25 +0700

    create UI

---
+++ src/views/DetailView.vue
@@ -0,0 +1,230 @@
+<template lang='pug'>
+.container
+  button.btn-back
+    i.fa-solid.fa-arrow-left
+    |  Back to Homepage
+  #country-name
+    .title
+      h1 Indonesia
+      img(
+        src='https://flagcdn.com/w320/id.png'
+      )
+    .another-name
+      p ID
+      p Republic of Indonesia
+      p Republik Indonesia
+  #location
+    .left-content
+      .display-box
+        .left-box
+          p.title LatLong
+          p.location -5.0, 120.0
+        .right-box
+          img(
+            src='@/assets/globe.svg'
+          )
+    .right-content
+      .display-box
+        p.capital Capital: Jakarta
+        p.region Region: Asia
+        p.subregion Subregion: South-Eastern Asia
+  #more-information
+    .left-content
+      .call-code
+        p.title Calling Code
+        p.number 62
+        p.more-information
+          span 1 country
+          |  with this calling code
+    .right-content
+      .currency
+        p.title Currency
+        p.currency-type IDR
+        p.more-information
+          span 1 country
+          |  with this currency
+</template>
+
+<script>
+export default {
+  setup () {
+  }
+}
+</script>
+
+<style scoped>
+  .container {
+    padding: 50px;
+  }
+
+  .btn-back {
+    color: #fff;
+    border: none;
+    width: 230px;
+    height: 50px;
+    border-radius: 10px;
+    background-color: #8362F2;
+    font-size: 18px;
+  }
+
+  #country-name {
+    margin-top: 50px;
+  }
+
+  #country-name .title {
+    display: flex;
+    align-items: center;
+  }
+
+  #country-name .title h1 {
+    font-size: 48px;
+  }
+
+  #country-name .title img {
+    width: 46px;
+    height: 30px;
+    margin-left: 20px;
+  }
+
+  #country-name .another-name {
+    font-size: 12px;
+    display: flex;
+    color: #fff;
+  }
+
+  #country-name .another-name p {
+    background: #8DD4CC;
+    margin: 5px 5px 5px 0;
+    padding: 5px 10px;
+    border-radius: 50px;
+  }
+
+  #location {
+    display: flex;
+  }
+
+  #location .left-content {
+    padding: 10px;
+    width: 50%;
+    height: 145px;
+  }
+
+  #location .left-content  .display-box {
+    display: flex;
+    overflow: hidden;
+  }
+
+  #location .left-content  .display-box .left-box {
+    width: 60%;
+    height: 100%;
+    padding: 20px;
+  }
+
+  #location .left-content  .display-box .left-box .title {
+    font-size: 18px;
+  }
+
+  #location .left-content  .display-box .left-box .location {
+    font-size: 48px;
+    margin: 10px 0;
+    color: #8362F2;
+    font-weight: 700;
+  }
+
+  #location .left-content  .display-box .right-box {
+    width: 40%;
+    height: 100%;
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  #location .left-content  .display-box .right-box img {
+    margin-top: 15px;
+    width: 204px;
+    height: 204px;
+  }
+
+  #location .right-content {
+    padding: 10px;
+    width: 50%;
+    height: 145px;
+  }
+
+  #location .right-content .display-box {
+    font-size: 18px;
+    padding: 20px;
+    line-height: 25px;
+  }
+
+  #location .left-content .display-box,
+  #location .right-content .display-box {
+    height: 100%;
+    box-shadow: -4px -4px 4px rgba(0, 0, 0, 0.02), 4px 4px 4px rgba(0, 0, 0, 0.02);
+  }
+
+  #more-information {
+    display: flex;
+  }
+
+  #more-information .left-content {
+    padding: 10px;
+    width: 50%;
+    height: 145px;
+  }
+
+  #more-information .left-content .call-code {
+    padding: 10px
+  }
+
+  #more-information .left-content .call-code .title {
+    font-size: 18px;
+  }
+
+  #more-information .left-content .call-code .number {
+    margin: 10px 0;
+    color: #8362F2;
+    font-size: 48px;
+    font-weight: 700;
+  }
+
+  #more-information .left-content .call-code .more-information {
+    font-size: 14px;
+  }
+
+  #more-information .left-content .call-code .more-information>span {
+    color: #8362F2;
+    text-decoration: underline;
+    cursor: pointer;
+  }
+
+  #more-information .right-content {
+    padding: 10px;
+    width: 50%;
+    height: 145px;
+  }
+
+  #more-information .right-content .currency {
+    padding: 10px
+  }
+
+  #more-information .right-content .currency .title {
+    font-size: 18px;
+  }
+
+  #more-information .right-content .currency .currency-type {
+    margin: 10px 0;
+    color: #8362F2;
+    font-size: 48px;
+    font-weight: 700;
+  }
+
+  #more-information .right-content .currency .more-information {
+    font-size: 14px;
+  }
+
+  #more-information .right-content .currency .more-information>span {
+    color: #8362F2;
+    text-decoration: underline;
+    cursor: pointer;
+  }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,132 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang='pug'>
+.container
+  .content
+    h1 Country
+    #search-bar
+      input(
+        type='text'
+        placeholder='Type any country name'
+        v-model='inputValue'
+        @keyup='changeHandle'
+      )
+      i.fa-solid.fa-magnifying-glass
+    #search-bar-recommendation(
+      v-if='inputValue !== ""'
+    )
+      .recommendation(
+        v-if='countryList.length > 0'
+        v-for='(value, key) in countryList'
+      )
+        .country-name(
+          :key='key'
+        )
+          p {{ value.name.common }}
+      .not-found(
+        v-else
+      )
+        p Data not found
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
+import { ref } from 'vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  setup () {
+    const inputValue = ref('')
+    const countryList = ref([])
+
+    const changeHandle = () => {
+      axios.get(`https://restcountries.com/v3.1/name/${inputValue.value}`)
+        .then(response => {
+          for (let i = 0; i < 5; i++) {
+            countryList.value.push(response.data[i])
+          }
+        })
+        .catch(err => console.log(err))
+    }
+
+    return {
+      countryList,
+      inputValue,
+      changeHandle
+    }
   }
 }
 </script>
+
+<style scoped>
+  .container {
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .content {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .content h1 {
+    font-size: 72px;
+  }
+
+  .content #search-bar {
+    position: relative;
+  }
+
+  .content #search-bar input {
+    margin-top: 20px;
+    border-radius: 10px;
+    border: 0.5px solid #C8C8C8;
+    padding: 0 20px 0 20px;
+    width: 700px;
+    height: 60px;
+    font-size: 18px;
+  }
+
+  .content #search-bar input:focus {
+    outline: none;
+    border: 1px solid rgba(131, 98, 242, 0.5);
+  }
+
+  .content #search-bar input::-webkit-input-placeholder {
+    color: #C8C8C8;
+  }
+
+  .content #search-bar i {
+    color: #C8C8C8;
+    position: absolute;
+    right: 2.5%;
+    top: 50%;
+  }
+
+  .content #search-bar-recommendation {
+    margin-top: 10px;
+    width: 700px;
+    max-height: 150px;
+    overflow: auto;
+    background: #FFFFFF;
+    box-shadow: -4px -4px 4px rgba(0, 0, 0, 0.5), 4px 4px 4px rgba(0, 0, 0, 0.5);
+    border-radius: 5px;
+    padding: 20px 0;
+  }
+
+  .content #search-bar-recommendation .recommendation {
+    padding: 10px 30px;
+  }
+
+  .content #search-bar-recommendation .recommendation:hover {
+    background-color: #F4F4F4;
+    cursor: pointer;
+  }
+
+  .content #search-bar-recommendation .not-found {
+    color: #FF0000;
+    padding: 10px 30px;
+  }
+</style>
